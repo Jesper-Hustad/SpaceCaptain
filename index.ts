9b656126171f97b5461f281deaeedc14646d358a
Jesper Hustad <jesterbe3@gmail.com>
2020-02-17 00:13:22 +0100

    fix not building js (aka. im retarded)

--- index.ts
+++ index.ts
@@ -76,7 +76,7 @@ class SpaceShip {
 
 
 canvas.addEventListener('touchmove', function(e) {
-    // Iterate through the list of touch points that changed
+    // Iterate through the list of touch points that changed 
     // since the last event and print each touch point's identifier.
     for (var i=0; i < e.changedTouches.length; i++) {
       console.log("changedTouches[" + i + "].identifier = " + e.changedTouches[i].identifier);
